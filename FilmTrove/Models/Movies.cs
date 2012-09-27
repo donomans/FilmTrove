c0e798a5f4a5011d6cf96b8871b2028d6d9ee3db
Donovan Sandey <donovan.sandey@gmail.com>
2012-09-27 00:23:37 -0500

    Initial with some of the theme implemented

---
+++ FilmTrove/Models/Movies.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace FilmTrove.Models
+{
+    public class Movie
+    {
+        public String MovieId { get; set; }
+        public String Title { get; set; }
+        public String Description { get; set; }
+        public MpaaRating MpaaRating { get; set; }
+        public String PosterId { get; set; }
+        public Int32 Year { get; set; }
+
+        public List<String> Director { get; set; } ///this needs ot be an id eventually
+        public List<String> Actors { get; set; }
+        public List<String> Writer { get; set; }
+
+        public Int64 Count { get; set; } ///need to shard this somehow?
+    }
+
+    public enum MpaaRating
+    {
+        R,
+        PG13,
+        PG,
+        G
+    }
+
+    public class MoviesContext : DbContext
+    {
+        public DbSet<Movie> Movies { get; set; }
+    }
+}

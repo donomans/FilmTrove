7e581c0ad3e0954d4a93c3012bfb6bcd7f37d41d
Donovan Sandey <donovan.sandey@gmail.com>
2012-09-27 17:02:23 -0500

    Theme work

--- FilmTrove/Models/Movies.cs
+++ FilmTrove/Models/Movies.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Data.Entity;
+using System.Collections;
 using System.Linq;
 using System.Web;
 
@@ -8,6 +11,7 @@ namespace FilmTrove.Models
 {
     public class Movie
     {
+        [Key]
         public String MovieId { get; set; }
         public String Title { get; set; }
         public String Description { get; set; }
@@ -15,15 +19,56 @@ namespace FilmTrove.Models
         public String PosterId { get; set; }
         public Int32 Year { get; set; }
 
-        public List<String> Director { get; set; } ///this needs ot be an id eventually
-        public List<String> Actors { get; set; }
-        public List<String> Writer { get; set; }
+        public ICollection<String> Genres { get; set; }
+
+        [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
+        public DateTime DateAdded { get; set; }
+
+        public virtual HashSet<String> Director { get; set; }
+        public virtual HashSet<String> Actors { get; set; }
+        public virtual HashSet<String> Writer { get; set; }
 
         public Int64 Count { get; set; } ///need to shard this somehow?
+
+        public override string ToString()
+        {
+            return Title;
+        }
+
+        public Movie()
+        {
+            Director = new HashSet<String>();
+            Actors = new HashSet<String>();
+            Writer = new HashSet<String>();
+        }
+    }
+
+    public class Person
+    {
+        [Key]
+        public String PersonId { get; set; }
+        public String Name { get; set; }
+
+        public virtual HashSet<String> Acted { get; set; }
+        public virtual HashSet<String> Directed { get; set; }
+        public virtual HashSet<String> Wrote { get; set; }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+
+        public Person()
+        {
+            Acted = new HashSet<String>();
+            Directed = new HashSet<String>();
+            Wrote = new HashSet<String>();
+        }
     }
 
     public enum MpaaRating
     {
+        Unrated,
         R,
         PG13,
         PG,
@@ -33,5 +78,42 @@ namespace FilmTrove.Models
     public class MoviesContext : DbContext
     {
         public DbSet<Movie> Movies { get; set; }
+        public DbSet<Person> People { get; set; }
+        
+        //protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        //{
+        //    modelBuilder.Entity<Movie>().
+        //      HasMany(c => c.Director).
+        //      WithMany(p => p.Directed).
+        //      Map(
+        //       m =>
+        //       {
+        //           m.MapLeftKey("MovieId");
+        //           m.MapRightKey("PersonId");
+        //           m.ToTable("MovieDirected");
+        //       });
+
+        //    modelBuilder.Entity<Movie>().
+        //      HasMany(c => c.Actors).
+        //      WithMany(p => p.Acted).
+        //      Map(
+        //       m =>
+        //       {
+        //           m.MapLeftKey("MovieId");
+        //           m.MapRightKey("PersonId");
+        //           m.ToTable("MovieActed");
+        //       });
+
+        //    modelBuilder.Entity<Movie>().
+        //      HasMany(c => c.Writer).
+        //      WithMany(p => p.Wrote).
+        //      Map(
+        //       m =>
+        //       {
+        //           m.MapLeftKey("MovieId");
+        //           m.MapRightKey("PersonId");
+        //           m.ToTable("MovieWrote");
+        //       });
+        //}
     }
 }

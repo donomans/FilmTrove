82c16338a3079d658c7f2c2d8eb5c9495555b36b
Donovan Sandey <donovan.sandey@gmail.com>
2012-10-02 17:38:38 -0500

    theme / login work

--- FilmTrove/Controllers/AccountController.cs
+++ FilmTrove/Controllers/AccountController.cs
@@ -7,6 +7,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebMatrix.WebData;
 
 namespace FilmTrove.Controllers
 {
@@ -26,6 +27,14 @@ namespace FilmTrove.Controllers
         {
             OAuthWebSecurity.RequestAuthentication("Google", Url.Action("GoogleLoginCallBack"));
             return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Logout()
+        {
+            WebSecurity.Logout();
+            return RedirectToAction("Index", "Home");
         }
 
         [AllowAnonymous]

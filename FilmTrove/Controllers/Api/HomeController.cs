3f90213ece817154d8dc79980508d99a08fc8ee2
Donovan Sandey <fronn1@gmail.com>
2013-05-24 14:52:12 -0500

    some cleanup and add newrelic

--- FilmTrove/Controllers/Api/HomeController.cs
+++ FilmTrove/Controllers/Api/HomeController.cs
@@ -14,26 +14,5 @@ namespace FilmTrove.Controllers.Api
         {
             return "I don't think this is where you want to be.";
         }
-
-        //// GET api/home/5
-        //public string Get(int id)
-        //{
-        //    return "value";
-        //}
-
-        //// POST api/home
-        //public void Post([FromBody]string value)
-        //{
-        //}
-
-        //// PUT api/home/5
-        //public void Put(int id, [FromBody]string value)
-        //{
-        //}
-
-        //// DELETE api/home/5
-        //public void Delete(int id)
-        //{
-        //}
     }
 }
